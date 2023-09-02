881207b4b8df97c85d2dd0fc1ecff0098cb2d77a
Ken <kabutukenneth3@gmail.com>
2023-09-02 12:51:13 +0300

    test

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn group work
+          Learn how the app works
         </a>
       </header>
     </div>
