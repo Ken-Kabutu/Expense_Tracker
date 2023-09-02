12ca1ac9548fd476ca91cf7cbe6bc770a4d77788
Ken <kabutukenneth3@gmail.com>
2023-09-02 16:06:57 +0300

    correcting error on seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,15 @@ ExpenseCategory.create(name: 'General')
 ExpenseCategory.create(name: 'Family and Friends')
 ExpenseCategory.create(name: 'Business Related')
 
-# Create sample expenses
-User.first.expenses.create(
+# Create a sample user with a password
+user = User.create(username: 'your_username', password_digest: 'your_password')
+
+# Create a sample expense associated with the user
+user.expenses.create(
   expense_category_id: ExpenseCategory.first.id,
   amount: 50.0,
   date: Date.today,
   description: 'Lunch with friends'
 )
-
 
 puts "...seeding complete"
