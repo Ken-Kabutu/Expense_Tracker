25d0283250f118635bc42ba5c01046f49a5df1b7
Ken <kabutukenneth3@gmail.com>
2023-09-02 15:02:57 +0300

    implementing CRUD operations under ExpensesController

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,3 +1,50 @@
 class ExpensesController < ApplicationController
-    # we implement CRUD actions
-end
+    before_action :set_expense, only: [:show, :update, :destroy]
+  
+    # GET /expenses
+    def index
+      @expenses = Expense.all
+      render json: @expenses
+    end
+  
+    # GET /expenses/1
+    def show
+      render json: @expense
+    end
+  
+    # POST /expenses
+    def create
+      @expense = Expense.new(expense_params)
+  
+      if @expense.save
+        render json: @expense, status: :created
+      else
+        render json: @expense.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # PATCH/PUT /expenses/1
+    def update
+      if @expense.update(expense_params)
+        render json: @expense
+      else
+        render json: @expense.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # DELETE /expenses/1
+    def destroy
+      @expense.destroy
+    end
+  
+    private
+  
+    def set_expense
+      @expense = Expense.find(params[:id])
+    end
+  
+    def expense_params
+      params.require(:expense).permit(:user_id, :expense_category_id, :amount, :date, :description)
+    end
+  end
+  
