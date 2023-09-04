51989aa7c7125a1fbc214e31fd58eb68304fef25
Ken <kabutukenneth3@gmail.com>
2023-09-04 17:18:46 +0300

    implement changes to routes to access users route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
   # route for devise
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
 
   
   resources :expenses, only: [:index, :show, :create, :update, :destroy]
