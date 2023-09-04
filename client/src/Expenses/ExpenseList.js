938b8da3dda856219913b44e51e879a97dbaf183
Ken <kabutukenneth3@gmail.com>
2023-09-04 17:50:59 +0300

    creating setState data

--- client/src/Expenses/ExpenseList.js
+++ client/src/Expenses/ExpenseList.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 
 function ExpenseForm() {
-    
+    const [expenseData, setExpenseData] = useState({
+        amount: '',
+        date: '',
+        description: '',
+    });
 }
