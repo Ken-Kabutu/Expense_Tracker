e245a481931ab310c351d1d44b710e47b3357d91
Ken <kabutukenneth3@gmail.com>
2023-09-04 18:31:27 +0300

    implementing handleSubmit

--- client/src/Expenses/ExpenseList.js
+++ client/src/Expenses/ExpenseList.js
@@ -15,5 +15,30 @@ function ExpenseForm() {
         });
     };
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        //implementing validation logic
+
+        //sending expense data to the api
+        try {
+            const response = await fetch('http://127.0.0.1:3000/expenses', {
+                method:'POST',
+                headers: {
+                    'Content-Type':"application/json",
+                },
+                body: JSON.stringify(expenseData),
+            });
+            if (response.ok) {
+                //expense created
+
+            } else {
+                //handling error
+            }
+        } catch (error) {
+            console.error('Error:' error);
+        }
+    };
+
+
     
 }
